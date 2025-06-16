9d8e9b2c74ccecea7ef2d897c0c21808e459e0e4
Surya Prakash <surya.forum@gmail.com>
2025-06-17 00:45:34 +0530

    initial-status

---
+++ Java Basics/BoilerTemplate.java
@@ -0,0 +1,8 @@
+public class BoilerTemplate {
+    public static void main(String[] args) {
+        // This is a boilerplate template for Java programs.
+        // You can replace the content of the main method with your own code.
+
+        System.out.println("Hello, World!"); // Replace this line with your code
+    }
+}
--- Java Basics/Surya.java
+++
@@ -1,6 +0,0 @@
-// Date: 2021-09-07
-public class Surya {
-    public static void main(String[] args) {
-        System.out.println("Hello, Surya Prakash");
-    }
-}

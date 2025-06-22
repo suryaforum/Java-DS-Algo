51aa5b1ca3e8d23d0ae034bc475e2f79101671d4
Surya Prakash <surya.forum@gmail.com>
2025-06-22 23:50:49 +0530

    practice-operators

---
+++ Java Basics/Test Programs/Test2.java
@@ -0,0 +1,20 @@
+public class Test2 {
+    public static void main(String[] args) {
+        int x = 200;
+        int y = 50;
+        int z = 100;
+
+        if (x > y && y > z) {
+            System.out.println("Hello");
+        }
+
+        if (z > y && z < x) {
+            System.out.println("Java");
+        }
+
+        if ((y + 200) < x && (y + 150) < z) {
+            System.out.println("Hello Java");
+
+        }
+    }
+}
